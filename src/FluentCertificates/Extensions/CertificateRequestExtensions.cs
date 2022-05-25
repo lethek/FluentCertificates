d676f37f039f27de319a6d2967b7a68cec31e55a
Michael Monsour <michael@lethek.net>
2022-05-25 14:33:35 +1000

    Added extension methods for exporting a CertificateRequest to a PEM string/file

---
+++ src/FluentCertificates/Extensions/CertificateRequestExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Pkcs;
+
+namespace FluentCertificates.Extensions;
+
+public static class CertificateRequestExtensions
+{
+    public static CertificateRequest ExportAsPem(this CertificateRequest certRequest, string path)
+    {
+        File.WriteAllText(path, certRequest.ConvertToBouncyCastle().ToPemString());
+        return certRequest;
+    }
+
+
+    public static string ToPemString(this CertificateRequest certRequest)
+    {
+        using var sw = new StringWriter();
+        var pem = new PemWriter(sw);
+        pem.WriteObject(certRequest.ConvertToBouncyCastle());
+        return sw.ToString();
+    }
+
+
+    public static Pkcs10CertificationRequest ConvertToBouncyCastle(this CertificateRequest certRequest)
+        => new(certRequest.CreateSigningRequest());
+}
