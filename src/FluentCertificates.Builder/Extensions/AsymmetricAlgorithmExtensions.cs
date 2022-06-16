c20c62e44f4c398ee5097f6a0cee8d4f2c168b50
Michael Monsour <michael@lethek.net>
2022-06-16 16:57:50 +1000

    Added some extension methods for exporting keys to PEM

---
+++ src/FluentCertificates.Builder/Extensions/AsymmetricAlgorithmExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+using FluentCertificates.Internals;
+
+namespace FluentCertificates.Extensions;
+
+public static class AsymmetricAlgorithmExtensions
+{
+    public static AsymmetricAlgorithm ExportAsPrivateKeyPem(this AsymmetricAlgorithm keys, string path)
+    {
+        File.WriteAllText(path, keys.ToPrivateKeyPemString());
+        return keys;
+    }
+
+
+    public static string ToPrivateKeyPemString(this AsymmetricAlgorithm keys)
+    {
+        using var sw = new StringWriter();
+        sw.Write(PemEncoding.Write("PRIVATE KEY", keys.ExportPkcs8PrivateKey()));
+        sw.Write('\n');
+        return sw.ToString();
+    }
+
+
+    public static AsymmetricAlgorithm ExportAsPublicKeyPem(this AsymmetricAlgorithm keys, string path)
+    {
+        File.WriteAllText(path, keys.ToPublicKeyPemString());
+        return keys;
+    }
+
+    
+    public static string ToPublicKeyPemString(this AsymmetricAlgorithm keys)
+    {
+        using var sw = new StringWriter();
+        sw.Write(PemEncoding.Write("PUBLIC KEY", keys.ExportSubjectPublicKeyInfo()));
+        sw.Write('\n');
+        return sw.ToString();
+    }
+}
